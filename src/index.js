d8f8bfc37966fb16f58a77b5ee7af05c822c90f6
wada-takurou <30787793+wada-takurou@users.noreply.github.com>
2021-06-06 17:09:37 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  console.log(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
